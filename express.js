8f6f2538b4a06be2561f8f3b2bb69231f8dbfab1
iemafzalhassan <iemafzalhassan@gmail.com>
2026-01-07 00:55:20 +0530

    Chores: initial commit

--- express.js
+++ express.js
@@ -17,7 +17,7 @@ router.get("/gist", gistCard);
 
 app.use("/api", router);
 
-const port = process.env.PORT || process.env.port || 9000;
+const port = process.env.PORT || process.env.port || 4600;
 app.listen(port, "0.0.0.0", () => {
   console.log(`Server running on port ${port}`);
 });
